d86378706964020cc147e92bad12c5699bcfb0dd
kaka272 <1028265636@qq.com>
2024-04-22 00:46:19 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ current_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $current_path
 
-bash start_ss_by_py_in_docker.sh
+docker run -d -p 9004:1314 kaka272/unisoc:ss0.1
 
 ssh -R 9003:127.0.0.1:9004  -N root@116.205.238.201
