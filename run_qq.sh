201a40f666c1b3e6778528c847a809b82a70a179
kaka272 <1028265636@qq.com>
2024-04-22 02:24:11 +0800

    Create run_qq.sh

---
+++ run_qq.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+current_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $current_path
+
+docker run -d -p 9004:1314 kaka272/unisoc:ss0.1
+
+ssh -R 9003:127.0.0.1:9004  -N root@106.54.54.7
