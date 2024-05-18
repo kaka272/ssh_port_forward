ad5aa054c47e7136e1e87bd7189cb17a01103644
kaka272 <1028265636@qq.com>
2024-05-18 22:22:06 +0800

    Update run_cpolar.sh

--- run_cpolar.sh
+++ run_cpolar.sh
@@ -1,2 +1,5 @@
 
 docker pull probezy/cpolar
+docker run -d -p 9004:1314 kaka272/unisoc:ss0.1
+cmd="cpolar authtoken YTg3YjIxYWUtMjdmOS00NTM0LTlkNmEtZTU0YzVjYmFlNGY0"
+docker run -id --network host --name cpolar probezy/cpolar bash -c "${cmd} ; cpolar tcp 9004"
