6ecc58d026ca20350a3ed2e4d39bf842a9017c3c
kaka272 <1028265636@qq.com>
2024-05-18 22:46:45 +0800

    Update run_cpolar.sh

--- run_cpolar.sh
+++ run_cpolar.sh
@@ -5,4 +5,4 @@ docker run -d -p 9004:1314 kaka272/unisoc:ss0.1
 echo 'input token'
 read token
 cmd="cpolar authtoken YjI4NzQxY2UtOWQyYy${token}MzQ2LTk5ZTQtZDM4ZDJmY2U5NDIx"
-docker run -id --network host --name cpolar probezy/cpolar bash -c "${cmd} ; cpolar tcp 9004"
+docker run -id --network host --entrypoint= --name cpolar probezy/cpolar bash -c "${cmd} ; cpolar tcp 9004"
