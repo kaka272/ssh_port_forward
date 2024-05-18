9cfc39a8c1b903fa288f44f4dace9b090a58b77d
kaka272 <1028265636@qq.com>
2024-05-19 00:10:01 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-current_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-cd $current_path
-
-docker run -d -p 9004:1314 kaka272/unisoc:ss0.1
-
-ssh -R 9003:127.0.0.1:9004  -N root@116.205.238.201
