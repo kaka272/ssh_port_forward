36bbacf8f369d143b22a4a7818826e2e915663e4
kaka272 <1028265636@qq.com>
2024-05-24 23:03:25 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -3,3 +3,5 @@
 sudo iptables -I PREROUTING -t nat  -p udp --dport 9019 -j DNAT --to 107.175.132.4:5170
 sudo iptables -t nat -A POSTROUTING -j MASQUERADE   # 数据回来时需要执行的操作
 sudo iptables -I FORWARD -p udp -d 107.175.132.4 --dport 5170 -j ACCEPT
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo sysctl -p
