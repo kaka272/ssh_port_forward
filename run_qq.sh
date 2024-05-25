72c580f5427e1a63b75f1d243cc85f53a6e1036b
kaka272 <1028265636@qq.com>
2024-05-25 21:16:57 +0800

    Update run_qq.sh

--- run_qq.sh
+++ run_qq.sh
@@ -7,3 +7,7 @@ cd $current_path
 docker run -d -p 9004:1314 kaka272/unisoc:ss0.1
 
 ssh -R 9003:127.0.0.1:9004  -N ubuntu@106.54.54.7
+
+#config 106.54.54.7 /etc/ssh/sshd_config
+#AllowTcpForwarding yes
+#GatewayPorts yes
