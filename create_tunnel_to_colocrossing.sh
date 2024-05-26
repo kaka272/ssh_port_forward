8791513e6f67b9b9b40431bda329338f8f4a8492
kaka272 <1028265636@qq.com>
2024-05-26 21:23:15 +0800

    new file

--- create_tunnel_to_colocrossing.sh
+++ create_tunnel_to_colocrossing.sh
@@ -38,17 +38,27 @@ else
 fi
 
 
-read -s -p "Please enter your password: " password
+read -s -p "Please enter your password: " machine_password
 
 
-screen -s bash -dmS colo_9003 -L -Logfile /tmp/colo_9003 ssh -L 0.0.0.0:9013:107.175.132.4:9003 root@107.175.132.4
+create_def_port_tunnel() {
+    local screen_name="$1"
+    local local_port="$2"
+    local remote_port="$3"
+    local screen_log="/tmp/${screen_name}.log"
 
-wait_for_string_in_log "/tmp/colo_9003" "yes/no" 30
+    rm -fr $screen_log
 
-screen -S colo_9003 -X stuff "yes$(printf \\r)"
+    screen -s bash -dmS $screen_name -L -Logfile $screen_log ssh -L 0.0.0.0:${local_port}:107.175.132.4:${remote_port} root@107.175.132.4
 
-wait_for_string_in_log "/tmp/colo_9003" "password:" 30
+    wait_for_string_in_log "$screen_log" "yes/no" 30
+    screen -S $screen_name -X stuff "yes$(printf \\r)"
 
-screen -S colo_9003 -X stuff "${password}$(printf \\r)"
+    wait_for_string_in_log "$screen_log" "password:" 30
+    screen -S $screen_name -X stuff "${password}$(printf \\r)"
+
+
+
+create_def_port_tunnel "colo_9003" 9013 9003
 
 
